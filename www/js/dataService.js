9c4ea7076ab05b36d154350551459cf9f4babf4b
Александр Николаев <alexandrnikolaev1@gmail.com>
2023-06-10 00:47:48 +0300

    Todos for capacitor

---
+++ www/js/dataService.js
@@ -0,0 +1,32 @@
+let todos = JSON.parse(localStorage.getItem("todos"));
+
+const saveTodos = (data) => {
+  localStorage.setItem("todos", JSON.stringify(data));
+};
+
+console.log(todos);
+
+if (!todos) {
+  todos = [];
+  saveTodos(todos);
+}
+
+const TodosService = {
+  save: saveTodos,
+
+  add: (todo) => {
+    todos.push(todo);
+    saveTodos(todos);
+  },
+
+  remove: (id) => {
+    const filtredTodos = todos.filter((todo, currentId) => currentId !== id);
+    todos = filtredTodos;
+    saveTodos(todos);
+  },
+
+  clear: () => {
+    todos = [];
+    saveTodos(todos);
+  },
+};
---
+++ www/js/script.js
@@ -0,0 +1,144 @@
+const navbarTitle = document.getElementById("navbar-title");
+const tabs = document.querySelectorAll(".tab");
+const tabbarItems = document.querySelectorAll(".tabbar-item");
+const formCreate = document.getElementById("form-create");
+const tabbar = document.getElementById("tabbar");
+
+//Settings
+const btnClearTodos = document.getElementById("btn-clear-todos");
+const switchTheme = document.getElementById("switch-theme");
+//const switchCircleTheme = switchTheme.querySelector(".switch-circle");
+
+const renderTodos = (data) => {
+  if (data.length === 0) {
+    tabs[0].innerHTML = "<h1>Список пуст</h1>";
+    return;
+  }
+
+  tabs[0].innerHTML = "";
+
+  data.map((todo, id) => {
+    const todoHtml = `
+              <div class="todo-item" onclick="openTodo('${todo.name}', '${todo.description}', '${todo.date}', ${id})">
+                  <h4>${todo.name}</h4>
+                  <span>${todo.date}</span>
+              </div>
+          `;
+    tabs[0].innerHTML += todoHtml;
+  });
+};
+
+const handleClickBtnClearTodos = () => {
+  const isAgree = confirm("Удалить все заметки?");
+  if (isAgree) {
+    TodosService.clear();
+    Notification.show("Заметки удалены!");
+    renderTodos(todos);
+  }
+};
+const handleChangeSwitchTheme = (event) => {
+  const checked = !JSON.parse(switchTheme.getAttribute("checked"));
+  if (checked) {
+    document.body.classList.add("dark-theme");
+    switchTheme.classList.add("switch-checked");
+    localStorage.setItem("theme", "dark");
+  } else {
+    document.body.classList.remove("dark-theme");
+    switchTheme.classList.remove("switch-checked");
+    localStorage.setItem("theme", "light");
+  }
+  switchTheme.setAttribute("checked", checked);
+};
+
+const handleClickTabbarItem = (event) => {
+  const targetTabId = event.currentTarget.getAttribute("tabid");
+  const tabName = event.currentTarget.getAttribute("tabname");
+
+  navbarTitle.innerText = tabName;
+
+  tabs.forEach((tab) => {
+    if (tab.id === targetTabId) {
+      tab.style.display = "block";
+    } else {
+      tab.style.display = "none";
+    }
+  });
+};
+
+const cleareForm = () => {
+  formCreate["name"].value = "";
+  formCreate["date"].value = "";
+  formCreate["description"].value = "";
+};
+
+const removeTodo = (id) => {
+  const isAgree = confirm("Вы уверены?");
+  if (isAgree) {
+    TodosService.remove(id);
+    Notification.show("Заметка удалены", 1000);
+    renderTodos(todos);
+    Modal.close();
+  }
+};
+const openTodo = (name, description, date, index) => {
+  console.log(name, description, date);
+
+  const content = `
+        <p>${description}</p>
+        <span>${date}</span>
+        <button class="mui-btn mui-btn--raised mui-btn--danger btn-remove" onclick="removeTodo (${index})">Удалить заметку</button>
+    `;
+
+  Modal.setTitle(name);
+  Modal.setContent(content);
+  Modal.open();
+};
+
+const handleSubmitFormCreate = (event) => {
+  event.preventDefault();
+
+  const todoName = formCreate["name"].value;
+  const todoDate = formCreate["date"].value;
+  const todoDescription = formCreate["description"].value;
+
+  if (todoName.length > 0) {
+    const todo = {
+      name: todoName,
+      date: todoDate,
+      description: todoDescription,
+    };
+
+    TodosService.add(todo);
+    cleareForm();
+    renderTodos(todos);
+
+    Notification.show("Заметка добавлена");
+  }
+};
+const checkTheme = () => {
+  const theme = localStorage.getItem("theme");
+  if (theme === "dark") {
+    document.body.classList.add("dark-theme");
+    switchTheme.classList.add("switch-checked");
+    switchTheme.setAttribute("checked", true);
+  }
+};
+const init = () => {
+  checkTheme();
+  renderTodos(todos);
+  tabbarItems.forEach((tabbarItem) =>
+    tabbarItem.addEventListener("click", handleClickTabbarItem)
+  );
+  formCreate.addEventListener("submit", handleSubmitFormCreate);
+  switchTheme.addEventListener("click", handleChangeSwitchTheme);
+  btnClearTodos.addEventListener("click", handleClickBtnClearTodos);
+  formCreate.querySelectorAll("input, textarea").forEach((field) => {
+    field.addEventListener("focus", () => (tabbar.style.display = "none"));
+  });
+
+  formCreate.querySelectorAll("input, textarea").forEach((field) => {
+    field.addEventListener("blur", () => (tabbar.style.display = "flex"));
+  });
+};
+
+init();
